04f02bc18ec8171bbd5a6fcad18b4f7fafe6c0ad
Orion Electron <electron.orion@gmail.com>
2021-11-28 16:48:02 +0545

    testing on apollo client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,162 @@
-import logo from './logo.svg';
+import {
+  useQuery,
+  useMutation,
+  gql,
+  useSubscription
+} from "@apollo/client";
 import './App.css';
+import { useState } from "react";
+
+
+const BOOKS_QUERY = gql`
+          query{
+              books(first: 4){
+                  pageInfo{
+                    startCursor
+                    endCursor
+                  }
+              edges{
+                  node{
+                      name
+                      id
+                      genre
+                  }
+                  cursor
+                }
+              }
+      }
+ `;
+
+const AUTHOR_MUTATION = gql`
+  mutation demo($name: String, $age: Int) {
+  author(name: $name, age: $age){
+    name,id,age
+  }
+}
+ 
+ `;
+
+ const GET_AUTHOR = gql `
+  query getAuthor($id: Int){
+    author(id: $id){
+      id,
+      name,
+      age
+    }
+  }
+ `;
+
+const AUTHOR_SUBSCRIPTION = gql `
+  subscription newAuthor($id: Int){
+    newAuthor(id: $id){
+      name,
+      age,
+      id
+    }
+      
+  }
+`;
+
+const GET_AUTHORS = gql `
+  query allAuthors{
+    authors{
+      name,
+      id,
+      age
+    }
+  }
+`;
+
+function AuthorMutation() {
+   const [state, setstate] = useState({name: '', age: -1})
+  const [mutateAuthor, { loading, error, data }] = useMutation(AUTHOR_MUTATION, {
+    update: (cache, {data: { author }}) => {
+      const data = cache.readQuery({query: GET_AUTHORS});
+      const author5 = cache.readFragment({
+        id: 'Author:5',
+        fragment: gql`
+          fragment myAuthor on Author{
+            id
+            name
+            age
+          }
+        `
+      });
+      console.log('Author:5', author5);
+      cache.writeQuery({ query: GET_AUTHORS, data: { authors: {author, ...data.authors}}});
+    }
+  });
+  if (loading) return <p> Loading... </p>;
+  if (error) return <p> Error :(</p>;
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(state);
+          mutateAuthor({ variables: { name: state.name, age: parseInt(state.age) }, onCompleted: (data) => {
+            console.log(data);
+          } },  )
+          
+          
+        }}
+      >
+        <input type="textbox" value={state.name} onChange={(e) => setstate({...state,name: e.target.value})} name="authorName"/> <br/>
+        <input type="number" value={state.age}  onChange={(e) => setstate({...state,age: e.target.value})} name="authorAge"/> <br/>
+
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+
+}
+
+function SubscriptionApp() {
+  const {data, loading } = useSubscription(AUTHOR_SUBSCRIPTION, {variables: {
+    id: 17
+  }});
+  if (loading) return "Loading subscription"
+  console.log(data);
+  return <h4>New Author: {data.newAuthor.name}</h4>;
+}
 
 function App() {
+
+  const { loading, error, data } = useQuery(GET_AUTHORS);
+
+  console.log(data);
+  if (loading) return <p> Loading... </p>;
+  if (error) return <p> Error :(</p>;
+
+/*
+        <ul>
+          {
+            data.authors.map((author) => {
+              return <li key={author.id}> {author.name} {author.age}</li>
+            })
+          }
+        </ul>
+        */
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Apollo graphql testing with apollo client</h1>
+        <ul>
+          {
+            data.authors.map((author) => {
+              return <li key={author.id}> {author.name} {author.age}</li>
+            })
+          }
+        </ul>
+        <AuthorMutation/>
+
       </header>
+
     </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true
+  }
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache()
+});
+
+render(
+
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
